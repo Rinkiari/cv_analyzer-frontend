8c9fea1fc4ec62453e0adad6354a39b2348c1ce6
Rinkiari <st112446@student.spbu.ru>
2026-04-18 18:03:01 +0300

    store fix

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import resumeReducer from '../slices/resumeSlice';
 import authReducer from '../slices/authSlice';
+import profileReducer from '../slices/profileSlice';
 
 export const store = configureStore({
   reducer: {
     resume: resumeReducer,
     auth: authReducer,
+    profile: profileReducer,
   },
 });
 
