29800888fbda4ec42d19037ba16b1c4a4b9f6fb4
Rinkiari <st112446@student.spbu.ru>
2026-05-09 03:43:29 +0300

    error page some additions

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,20 @@
+import { Link } from 'react-router';
 import styles from './ErrorPage.module.scss';
 
 import error3 from '../../assets/404_error3.png';
 
 const ErrorPage = () => {
   return (
-    <>
-      <div className={styles.icon_container}>
-        <img src={error3} alt="404 error" />
-      </div>
-    </>
+    <div className={styles.icon_container}>
+      <img src={error3} alt="404 error" />
+      <h1 className={styles.title}>Страница не найдена</h1>
+      <p className={styles.subtitle}>
+        Похоже, такой страницы нет или ссылка устарела.
+      </p>
+      <Link to="/" className={styles.homeLink}>
+        На главную
+      </Link>
+    </div>
   );
 };
 
