3b6fa93e923805229edf4c56fa1175372b14df2d
Rinkiari <st112446@student.spbu.ru>
2026-05-09 03:10:22 +0300

    manualFields component bug fix

--- src/components/ManualFields/ManualFields.jsx
+++ src/components/ManualFields/ManualFields.jsx
@@ -1,79 +1,37 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { updateManualField } from '../../redux/slices/resumeSlice';
 import styles from './ManualFields.module.scss';
 
+const FIELDS = [
+  { key: 'fullName', label: 'ФИО' },
+  { key: 'position', label: 'Позиция' },
+  { key: 'skills', label: 'Навыки' },
+  { key: 'experience', label: 'Опыт' },
+  { key: 'education', label: 'Образование' },
+  { key: 'aboutYourself', label: 'О себе' },
+];
+
 function ManualFields() {
   const dispatch = useDispatch();
+  const manualForm = useSelector((state) => state.resume.manualForm);
 
-  const handleInput = (field) => (e) => {
-    dispatch(
-      updateManualField({
-        field,
-        value: e.currentTarget.innerText,
-      }),
-    );
+  const handleChange = (field) => (e) => {
+    dispatch(updateManualField({ field, value: e.target.value }));
   };
 
   return (
     <div className={styles.manual_wrapper}>
-      <div className={styles.fio_field}>
-        <p>ФИО</p>
-        <div
-          className={styles.editable}
-          contentEditable
-          onInput={handleInput('fullName')}
-          suppressContentEditableWarning
-        />
-      </div>
-
-      <div className={styles.fio_field}>
-        <p>Позиция</p>
-        <div
-          className={styles.editable}
-          contentEditable
-          onInput={handleInput('position')}
-          suppressContentEditableWarning
-        />
-      </div>
-
-      <div className={styles.fio_field}>
-        <p>Навыки</p>
-        <div
-          className={styles.editable}
-          contentEditable
-          onInput={handleInput('skills')}
-          suppressContentEditableWarning
-        />
-      </div>
-
-      <div className={styles.fio_field}>
-        <p>Опыт</p>
-        <div
-          className={styles.editable}
-          contentEditable
-          onInput={handleInput('experience')}
-          suppressContentEditableWarning
-        />
-      </div>
-      <div className={styles.fio_field}>
-        <p>Образование</p>
-        <div
-          className={styles.editable}
-          contentEditable
-          onInput={handleInput('education')}
-          suppressContentEditableWarning
-        />
-      </div>
-
-      <div className={styles.fio_field}>
-        <p>О себе</p>
-        <div
-          className={styles.editable}
-          contentEditable
-          onInput={handleInput('aboutYourself')}
-          suppressContentEditableWarning
-        />
-      </div>
+      {FIELDS.map(({ key, label }) => (
+        <div key={key} className={styles.fio_field}>
+          <p>{label}</p>
+          <input
+            type="text"
+            className={styles.editable}
+            value={manualForm[key] ?? ''}
+            onChange={handleChange(key)}
+          />
+        </div>
+      ))}
     </div>
   );
 }
